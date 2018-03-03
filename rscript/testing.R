dcaa59dd1a002c1557ec0456da93249e8d5a6927
Rose Dawson <Rose@roses-mbp-2.coa-domain.com>
2018-03-03 18:30:46 -0500

    (Committed to the wrong branch.)
    Added Levene's test for variance in leaf area.

--- rscript/testing.R
+++ rscript/testing.R
@@ -43,3 +43,19 @@ chit<-chisq.test(Pref_t)
 chit$expected
 
 #Pref$habitat<-if
+
+#######How much does leaf area vary by site?——Levene's test#######
+library(car)
+hist(habitat$avg.leaf.area)
+#Trim habtiat to reflect the sites we're actually working with
+habitat = subset(habitat, site=="P1"|site=="P5"|site=="TIR"|site=="P13"|site=="P15"|site=="P16"|site=="POT4")
+leveneTest(habitat$avg.leaf.area, habitat$site)
+#P = 0.0948
+#We fail to reject the null that there is difference in population variance but this is fairly close.
+#How strong is this test compared to our data?
+
+#####How much does leaf area vary by habitat?####
+leveneTest(habitat$avg.leaf.area, habitat$habitat)
+#P = 0.05382
+#We fail to reject the null that there is a difference in population variance.
+#However, it's a very close thing!
