66c892b32b7a56c8912b1a70ee2d9c7772c004d3
Rose Dawson <Rose@roses-mbp-2.coa-domain.com>
2018-03-13 13:20:06 -0400

    Added bootstrapped data for leaf area test

--- rscript/testing.R
+++ rscript/testing.R
@@ -55,7 +55,48 @@ leveneTest(habitat$avg.leaf.area, habitat$site)
 #How strong is this test compared to our data?
 
 #####How much does leaf area vary by habitat?####
-leveneTest(habitat$avg.leaf.area, habitat$habitat)
 #P = 0.05382
 #We fail to reject the null that there is a difference in population variance.
 #However, it's a very close thing!
+
+####Do frogs prefer certain leaf areas, given the available area at each site?###
+
+HP1boot=NULL
+HP13boot=NULL
+HP15boot=NULL
+HP16boot=NULL
+HP5boot=NULL
+HPOT4boot=NULL
+
+EP1boot=NULL
+EP13boot=NULL
+EP15boot=NULL
+EP16boot=NULL
+EP5boot=NULL
+EPOT4boot=NULL
+
+TP1boot=NULL
+TP13boot=NULL
+TP15boot=NULL
+TP16boot=NULL
+TP5boot=NULL
+TPOT4boot=NULL
+#Replace relevant fields in code for each series of bootstrapping
+corrData <- unlist(P5[,c("leaf.area1", "leaf.area2", "leaf.area3")], use.names = FALSE)
+long = length(pref_raw$site == "P5"&pref_raw$species=="ESPPRO")
+
+for (i in c(1:9999)) {
+  samp=sample(corrData,size=length(which(pref_raw$site=="P5"&pref_raw$species=="ESPPRO")),replace=T)
+  EP5boot[i]=samp
+}
+
+#Write CSVs
+write.csv(HP1boot, "data/HP1boot.csv");write.csv(HP13boot, "data/HP13boot.csv");write.csv(HP15boot, "data/HP15boot.csv");write.csv(HP16boot, "data/HP16boot.csv");write.csv(HP5boot, "data/HP5boot.csv");write.csv(HPOT4boot, "data/HPOT4boot.csv")
+write.csv(EP1boot, "data/EP1boot.csv");write.csv(EP13boot, "data/EP13boot.csv");write.csv(EP15boot, "data/EP15boot.csv"); write.csv(EP16boot, "data/EP16boot.csv");write.csv(EP5boot, "data/EP5boot.csv");write.csv(EPOT4boot, "data/EPOT4boot.csv")
+write.csv(TP1boot, "data/TP1boot.csv"); write.csv(TP13boot, "data/TP13boot.csv"); write.csv(TP15boot, "data/TP15boot.csv"); write.csv(TP16boot, "data/TP16boot.csv"); write.csv(TP5boot, "data/TP5boot.csv"); write.csv(TPOT4boot, "data/TPOT4boot.csv")
+
+###Create histograms###
+par(mfrow=c(2,3))
+hist(log(HP1boot)); hist(log(HP13boot));hist(log(HP15boot));hist(log(HP5boot))
+hist(log(EP1boot)); hist(log(EP16boot));hist(log(EP5boot));hist(log(EPOT4boot))
+hist(log(TP1boot)); hist(log(TP13boot));hist(log(TP15boot));hist(log(TP16boot));hist(log(TP5boot));hist(log(TPOT4boot))
